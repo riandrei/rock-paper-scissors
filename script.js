1f380c4c04b0d3397084b7bdf2146c4b8870f7aa
Riandrei Casañas <riandrei.casanas@gmail.com>
2022-04-26 15:52:16 +0800

    Completed the last function
    The game is now a 5 round game and announces the winner at the end.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   let random = Math.floor(Math.random() * 3) + 1;
 
@@ -15,22 +18,28 @@ function playRound(playerSelection, computerSelection) {
     if (computerSelection() == `rock`) {
       return `Draw! Rock is tied with Rock`;
     } else if (computerSelection() == `paper`) {
+      computerScore++;
       return `You Lose! Rock loses against Paper`;
     } else {
+      playerScore++;
       return `You Win! Rock beats Scissors`;
     }
   } else if (playerSelection.toLowerCase() == `paper`) {
     if (computerSelection() == `rock`) {
+      playerScore++;
       return `You Win! Paper beats Rock`;
     } else if (computerSelection() == `paper`) {
       return `Draw! Paper is tied with Paper`;
     } else {
+      computerScore++;
       return `You Lose! Paper loses against Scissors`;
     }
   } else if (playerSelection.toLowerCase() == `scissors`) {
     if (computerSelection() == `rock`) {
+      computerScore++;
       return `You Lose! Scissors loses against Rock`;
     } else if (computerSelection() == `paper`) {
+      playerScore++;
       return `You Win! Scissors beats Paper`;
     } else {
       return `Draw! Scissors is tied with Scissors`;
@@ -39,3 +48,19 @@ function playRound(playerSelection, computerSelection) {
     return `You entered an invalid choice. Please try again.`;
   }
 }
+
+function game() {
+  for (let i = 1; i < 6; i++ || playerScore < 3 || computerScore < 3) {
+    let playerSelection = prompt(`Rock, Paper, or Scissors?`);
+    console.log(playRound(playerSelection, computerPlay));
+  }
+  if (playerScore > computerScore) {
+    console.log(`You Won! The score is ${playerScore} to ${computerScore}.`);
+  } else if (playerScore < computerScore) {
+    console.log(`You Lose! The score is ${playerScore} to ${computerScore}`);
+  } else {
+    console.log(`It's a draw?`);
+  }
+}
+
+game();
