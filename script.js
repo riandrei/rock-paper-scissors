51385f45fb5b4f835608e864e8de49920dd24c56
Riandrei Casañas <riandrei.casanas@gmail.com>
2022-04-26 15:25:10 +0800

    Finished the function that plays one round of rock paper scissors

--- script.js
+++ script.js
@@ -2,12 +2,40 @@ function computerPlay() {
   let random = Math.floor(Math.random() * 3) + 1;
 
   if (random == 1) {
-    return `Rock`;
+    return `rock`;
   } else if (random == 2) {
-    return `Paper`;
+    return `paper`;
   } else {
-    return `Scissors`;
+    return `scissors`;
   }
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() == `rock`) {
+    if (computerSelection() == `rock`) {
+      return `Draw! Rock is tied with Rock`;
+    } else if (computerSelection() == `paper`) {
+      return `You Lose! Rock loses against Paper`;
+    } else {
+      return `You Win! Rock beats Scissors`;
+    }
+  } else if (playerSelection.toLowerCase() == `paper`) {
+    if (computerSelection() == `rock`) {
+      return `You Win! Paper beats Rock`;
+    } else if (computerSelection() == `paper`) {
+      return `Draw! Paper is tied with Paper`;
+    } else {
+      return `You Lose! Paper loses against Scissors`;
+    }
+  } else if (playerSelection.toLowerCase() == `scissors`) {
+    if (computerSelection() == `rock`) {
+      return `You Lose! Scissors loses against Rock`;
+    } else if (computerSelection() == `paper`) {
+      return `You Win! Scissors beats Paper`;
+    } else {
+      return `Draw! Scissors is tied with Scissors`;
+    }
+  } else {
+    return `You entered an invalid choice. Please try again.`;
+  }
+}
