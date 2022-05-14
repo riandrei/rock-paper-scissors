8f7ccd6b7f68752fd629804e0183bc89fb3f2f2e
Riandrei Casañas <riandrei.casanas@gmail.com>
2022-05-14 18:40:31 +0800

    Modified the code to be more concise and easier to read.

--- script.js
+++ script.js
@@ -6,60 +6,52 @@ function computerPlay() {
 
   if (random == 1) {
     return `rock`;
-  } else if (random == 2) {
-    return `paper`;
-  } else {
-    return `scissors`;
   }
+  if (random == 2) {
+    return `paper`;
+  }
+  return `scissors`;
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() == `rock`) {
-    if (computerSelection() == `rock`) {
-      return `Draw! Rock is tied with Rock`;
-    } else if (computerSelection() == `paper`) {
-      computerScore++;
-      return `You Lose! Rock loses against Paper`;
-    } else {
-      playerScore++;
-      return `You Win! Rock beats Scissors`;
-    }
-  } else if (playerSelection.toLowerCase() == `paper`) {
-    if (computerSelection() == `rock`) {
-      playerScore++;
-      return `You Win! Paper beats Rock`;
-    } else if (computerSelection() == `paper`) {
-      return `Draw! Paper is tied with Paper`;
-    } else {
-      computerScore++;
-      return `You Lose! Paper loses against Scissors`;
-    }
-  } else if (playerSelection.toLowerCase() == `scissors`) {
-    if (computerSelection() == `rock`) {
-      computerScore++;
-      return `You Lose! Scissors loses against Rock`;
-    } else if (computerSelection() == `paper`) {
-      playerScore++;
-      return `You Win! Scissors beats Paper`;
-    } else {
-      return `Draw! Scissors is tied with Scissors`;
-    }
-  } else {
-    return `You entered an invalid choice. Please try again.`;
+function playRound(playerChoice, computerChoice) {
+  const winConditions =
+    (playerChoice.toLowerCase() == `paper` && computerChoice == `rock`) ||
+    (playerChoice.toLowerCase() == `rock` && computerChoice == `scissors`) ||
+    (playerChoice.toLowerCase() == `scissors` && computerChoice == `paper`);
+  const invalidChoices =
+    playerChoice.toLowerCase() == `rock` ||
+    playerChoice.toLowerCase() == `paper` ||
+    playerChoice.toLowerCase() == `scissors`;
+
+  if (!invalidChoices) {
+    return `You entered an invalid choice`;
   }
+
+  if (playerChoice.toLowerCase() == computerChoice) {
+    return `Draw! ${playerChoice.toLowerCase()} and ${computerChoice} is tied.`;
+  }
+
+  if (winConditions) {
+    playerScore++;
+    return `You win! ${playerChoice.toLowerCase()} beats ${computerChoice}`;
+  }
+
+  computerScore++;
+  return `You lose! ${playerChoice.toLowerCase()} loses against ${computerChoice}`;
 }
 
 function game() {
   for (let i = 1; i < 6; i++ || playerScore < 3 || computerScore < 3) {
-    let playerSelection = prompt(`Rock, Paper, or Scissors?`);
-    console.log(playRound(playerSelection, computerPlay));
+    const playerChoice = prompt(`Rock, Paper, or Scissors?`);
+    console.log(playRound(playerChoice, computerPlay()));
   }
+
   if (playerScore > computerScore) {
     console.log(`You Won! The score is ${playerScore} to ${computerScore}.`);
   } else if (playerScore < computerScore) {
     console.log(`You Lose! The score is ${playerScore} to ${computerScore}`);
   } else {
-    console.log(`It's a draw?`);
+    console.log(`It's a draw`);
   }
 }
 
